902c756c5d118eb6cbb096c39608e3df0d587fc5
Evgeniy <48185066+TzeenchH@users.noreply.github.com>
2019-10-19 00:56:43 +0300

    Add files via upload

---
+++ DelegateCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace HelpersLib
+{ 
+    public class DelegateCommand<T> : ICommand
+    {
+        protected Action<T> _execute;
+        protected Func<T, bool> _canExecute;
+
+        protected DelegateCommand() { }
+
+        public DelegateCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException(nameof(execute));
+            _execute = execute;
+            _canExecute = canExecute ?? (p => true);
+        }
+        public DelegateCommand(Action<T> execute) : this(execute, (Func<T, bool>)null) { }
+        public DelegateCommand(Action<T> execute, Func<bool> canExecute) : this(execute, p => canExecute()) { }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter) => _execute((T)parameter);
+        public bool CanExecute(object parameter) => _canExecute((T)parameter);
+    }
+
+    public class DelegateCommand : DelegateCommand<object>
+    {
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute) : base(execute, canExecute) { }
+
+        public DelegateCommand(Action execute) : base(p => execute()) { }
+        public DelegateCommand(Action execute, Func<bool> canExecute) : base(p => execute(), p => canExecute()) { }
+
+        public DelegateCommand(Action<object> execute) : base(execute) { }
+        public DelegateCommand(Action<object> execute, Func<bool> canExecute) : base(execute, canExecute) { }
+    }
+}
---
+++ ObservableBase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HelpersLib
+{
+    public abstract class ObservableBase : INotifyPropertyChanged
+    {
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public virtual void OnPropertyChanged(params string[] propertyNames)
+        {
+            foreach (var property in propertyNames)
+                OnPropertyChanged(property);
+        }
+
+        protected bool SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
